875e211afabd175a542164313433214eccc4304e
K. Cameron Majidzadeh <katecm00@gmail.com>
2019-02-03 00:18:21 -0500

    Finished Styles and Dragging for Spell Casting
    -Added hints and event notices
    -ammended JavaScript to prevent incorrect node appends
    -ammended JavaScript to prevent incorrect color/style changes
    -Finished build info and template for first "level"
    
    TODO:
    -validate drag input
    -trigger canvas events
    implement timer
    -build out levels 2 and 3 and implement level switching system

--- spell-caster.js
+++ spell-caster.js
@@ -1,44 +1,94 @@
 //Variable declaration
 
 
-
 const spell = document.getElementsByClassName('spell')[0];
+
+const spells = document.getElementsByClassName('spell');
+const dropzones = document.getElementsByClassName('dropzone');
+const spell_docks = document.getElementsByClassName('spell-dock');
 //
 
 var dragged;
 
-// for(const dropzone of dropzones){
-//     dropzone.addEventListener('dragstart', dragStart);
-//     dropzone.addEventListener('dragend', dragEnd);
-//     dropzone.addEventListener('dragover', dragOver);
-//     dropzone.addEventListener('dragenter', dragEnter);
-//     dropzone.addEventListener('dragleave', dragLeave);
-//     //dropzone.addEventListener('drop', drop);
-//     //container.addEventListener('dragexit', dragExit);
-// }
+for(const dropzone of dropzones){
+    dropzone.addEventListener("dragenter", function(event) {
+            // highlight potential drop target when the draggable element enters it
+            if ( event.target.className == "dropzone" ) {
+                event.target.classList.toggle("entered-dropzone");
+            }    
+    }, false);
+    dropzone.addEventListener("dragleave", function(event) {
+              // reset background of potential drop target when the draggable element leaves it
+              if ( event.target.className != "dropzone" ) {
+                if ( event.target.classList.contains("entered-dropzone")){ 
+                    event.target.classList.toggle("entered-dropzone");
+                }
+                
+              }
+    },false);
+    dropzone.addEventListener("drop", function(event) {
+            // prevent default action (open as link for some elements)
+            event.preventDefault();
+            // move dragged elem to the selected drop target
+            console.log(`event.target.className: ${event.target.className}`);
+            if ( event.target.classList.contains("dropzone") || event.target.classList.contains("spell-dock")) {
+                event.target.classList.toggle("entered-dropzone");
+        
+                //removeChild( dragged );
+                dragged.parentNode.removeChild( dragged );
+                // .removeChild( dragged );
+                event.target.appendChild( dragged );
+               
+            }   
+    }, false);
+}
+for(const dock of spell_docks){
+    dock.addEventListener("dragenter", function(event) {
+            // highlight potential drop target when the draggable element enters it
+            if ( event.target.className == "dropzone" ) {
+                event.target.classList.toggle("entered-dropzone");
+            }    
+    }, false);
+    dock.addEventListener("dragleave", function(event) {
+              // reset background of potential drop target when the draggable element leaves it
+            if ( event.target.className != "dropzone" ) {
+                  if ( event.target.classList.contains("entered-dropzone")){ 
+                    event.target.classList.toggle("entered-dropzone");
+                }
+            }
+    },false);
+    dock.addEventListener("drop", function(event) {
+            // prevent default action (open as link for some elements)
+            event.preventDefault();
+            // move dragged elem to the selected drop target
+            console.log(`event.target.className: ${event.target.className}`);
+            if ( event.target.classList.contains("dropzone") || event.target.classList.contains("spell-dock")) {
+                event.target.classList.toggle("entered-dropzone");
+        
+                //removeChild( dragged );
+                dragged.parentNode.removeChild( dragged );
+                // .removeChild( dragged );
+                event.target.appendChild( dragged );
+               
+            }   
+    }, false);
+}
+for(const spell of spells){
+    // document.addEventListener("drag", function( event ) {
+    spell.addEventListener("dragstart", function(event) {
+            console.log("check spell is dragged")
+            // store a reference on the dragged elem
+            dragged = event.target;
+            //being-dragged CSS sets style
+            event.target.classList.toggle('spell-dragging');
+        }, false);
+    //dragend
+    spell.addEventListener("dragend", function(event) {
+        // reset the transparency    
+        event.target.classList.toggle('spell-dragging');
+        }, false);
+}
 
-
-
-// document.addEventListener("drag", function( event ) {
-
-//   }, false);
-
-document.addEventListener("dragstart", function(event) {
-    // store a reference on the dragged elem
-    dragged = event.target;
-    //being-dragged CSS sets style
-    event.target.classList.toggle('spell-dragging');
-    console.log(`DRAG-START: event.target.classList: ${event.target.classList}`);
-//       // make it half transparent
-//       event.target.style.opacity = .5;
-}, false);
-
-//dragend
-document.addEventListener("dragend", function(event) {
-    // reset the transparency    
-    event.target.classList.toggle('spell-dragging');
-    console.log(`DRAG-END: event.target.classList: ${event.target.classList}`)
-}, false);
 
 //drag over
 /* events fired on the drop targets */
@@ -46,142 +96,3 @@ document.addEventListener("dragover", function(event) {
           // prevent default to allow drop
           event.preventDefault();
 }, false);
-
-// dropping the div into the bigger div
-document.addEventListener("dragenter", function(event) {
-    // highlight potential drop target when the draggable element enters it
-    if ( event.target.className == "dropzone" ) {
-        event.target.classList.toggle("entered-dropzone");
-        console.log(`DRAG-ENTER: event.target.classList: ${event.target.classList}`);
-    }    
-}, false);
-
-document.addEventListener("dragleave", function(event) {
-      // reset background of potential drop target when the draggable element leaves it
-     console.log('DRAG LEFT')
-      if ( event.target.className != "dropzone" ) {
-        event.target.classList.toggle("entered-dropzone");
-        console.log(`DRAG-LEAVE: event.target.classList: ${event.target.classList}`);
-      }
-},false);
-
-document.addEventListener("drop", function(event) {
-    // prevent default action (open as link for some elements)
-    event.preventDefault();
-    // move dragged elem to the selected drop target
-    console.log(`event.target.className: ${event.target.className}`);
-    if ( event.target.classList.contains("entered-dropzone")) {
-        event.target.classList.toggle("entered-dropzone");
-
-        console.log("HERE?");
-        //removeChild( dragged );
-        dragged.parentNode.removeChild( dragged );
-        // .removeChild( dragged );
-        event.target.appendChild( dragged );
-        console.log(`PARENT NODE: ${dragged.parentNode}`);
-       
-    }   
-}, false);
-
-
-
-
-
-
-
-///<<<<<------- CORE IDEA HERE !!!! ////// ----->>>>>
-
-// const spell = document.getElementsByClassName('spell')[0];
-// const dropzones = document.getElementsByClassName('dropzone');
-
-// var dragged;
-// var box;
-
-// for(const dropzone of dropzones){
-//     dropzone.addEventListener('dragstart', dragStart);
-//     dropzone.addEventListener('dragend', dragEnd);
-//     dropzone.addEventListener('dragover', dragOver);
-//     dropzone.addEventListener('dragenter', dragEnter);
-//     dropzone.addEventListener('dragleave', dragLeave);
-//     //dropzone.addEventListener('drop', drop);
-//     //container.addEventListener('dragexit', dragExit);
-// }
-
-
-
-// // document.addEventListener("drag", function( event ) {
-
-// //   }, false);
-
-// function dragStart() {
-//     // store a reference on the dragged elem
-//     dragged = event.target;
-//     //being-dragged CSS sets style
-//     event.target.classList.toggle('spell-dragging');
-//     console.log(`DRAG-START: event.target.classList: ${event.target.classList}`);
-// //       // make it half transparent
-// //       event.target.style.opacity = .5;
-// }
-
-// function dragEnd() {
-//     // reset the transparency    
-//     event.target.classList.toggle('spell-dragging');
-//     console.log(`DRAG-END: event.target.classList: ${event.target.classList}`)
-// }
-
-// /* events fired on the drop targets */
-// function dragOver() {
-//           // prevent default to allow drop
-//           event.preventDefault();
-// }
-
-// // dropping the div into the bigger div
-// function dragEnter() {
-//     // highlight potential drop target when the draggable element enters it
-//     event.target.classList.toggle("entered-dropzone");
-//     console.log(`DRAG-ENTER: event.target.classList: ${event.target.classList}`);
-// }
-
-// function dragLeave() {
-//       // reset background of potential drop target when the draggable element leaves it
-//      console.log('DRAG LEFT')
-//       if ( event.target.className != "dropzone" ) {
-//         event.target.classList.toggle("entered-dropzone");
-//         console.log(`DRAG-LEAVE: event.target.classList: ${event.target.classList}`);
-//       }
-// }
-
-// document.addEventListener("drop", function(event) {
-//     // prevent default action (open as link for some elements)
-//     event.preventDefault();
-//     // move dragged elem to the selected drop target
-//     console.log('HEEEELLOOOOOO');
-//     //event.target.classList.toggle("entered-dropzone");
-//     console.log(`event.target.className: ${event.target.className}`);
-//     if ( event.target.classList.contains("entered-dropzone")) {
-//         console.log("HERE?");
-//         //removeChild( dragged );
-//         dragged.parentNode.removeChild( dragged );
-//         // .removeChild( dragged );
-//         event.target.appendChild( dragged );
-//         console.log(`PARENT NODE: ${dragged.parentNode}`);
-       
-//     }   
-// }, false);
-
-// for(const container of containers){
-//     container.addEventListener("dragover", dragover);
-//     container.addEventListener("dragenter", dragenter);
-//     container.addEventListener("drop", drop);
-// }
-
-// function dragover(e){
-
-//     e.preventDefault()
-// }
-// function dragenter(e){
-//     e.preventDefault()
-// }
-// function drop(){
-//     this.append(spell)
-// }
